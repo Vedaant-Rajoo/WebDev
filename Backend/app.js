cf49a2fe347c5bfcdc247999ca30fe023eb03b8c
Vedaant-rajoo <vedaant12345@gmail.com>
2019-01-28 23:55:26 +0530

    Backend/app:

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,12 @@ var app = express();
 // "/" => "Hi there!!"
 app.get("/", function(req, res) {
     res.send("Hi There!! ");
-
 });
+// "/bye"=> "goodbye
+app.get("/bye", function(req, res) {
+    res.send("Goodbye");
+});
+
 app.listen(3000, function() {
     console.log("serving helloworld on port 3000");
 });
