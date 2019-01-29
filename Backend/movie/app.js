90005995aaf1692b8448eb906bc98fd700566e10
Vedaant-rajoo <vedaant12345@gmail.com>
2019-01-29 12:31:33 +0530

    Backend/movie/app: create

---
+++ Backend/movie/app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(4000, function() {
+    console.log("Movie app has started  !");
+});
